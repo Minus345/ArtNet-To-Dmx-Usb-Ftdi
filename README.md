397774272f67eafddf88e1478a838b6977487b2d
Minus345 <106837932+Minus345@users.noreply.github.com>
2023-05-25 22:33:16 +0200

    add command line parameters

--- README.md
+++ README.md
@@ -1,5 +1,30 @@
 # Fidi-Dmx-Usb-To-ArtNet
-Java Software that sends the ArtNet data to the dmx ftid usb dongel
-### Libs Used:  
+
+Java Software that sends the ArtNet data to the dmx ftid usb dongel  
+Build for https://www.amazon.de/DSD-TECH-RS485-Adapter-Lichtsteuerungskabel-Schwarz/dp/B07WV6P5W6
+
+Tested on:
+* windows 10
+* ~~raspberry pi~~ ( in development should work)
+
+### Requirements
+* java 19
+* ftdi driver
+
+### Parameters
+
+`java -jar FtdiDmx.jar`
+
+| Parameter              |                   Usage                   |
+|------------------------|:-----------------------------------------:|
+| -d, --debug            |      shows the debug artnet packetes      |
+| -h, --help             |          shows the help message           |
+| -i, --interface <ethx> | network interface you recieve your artnet |
+| -l, --list-interfaces  |       lists the network interfaces        |
+| -s, --subnet <0>       |               ArtNet Subnet               |
+| -u, --universe <0>     |              ArtNet Universe              |
+
+### Libs Used:
+
 - Dmx4Artists https://github.com/jaysonh/Dmx4Artists
 - ArtNet4J https://github.com/cansik/artnet4j
--- src/main/java/org/dmx/Main.java
+++ src/main/java/org/dmx/Main.java
@@ -2,6 +2,7 @@ package org.dmx;
 
 import ch.bildspur.artnet.ArtNetClient;
 import com.jaysonh.dmx4artists.DMXControl;
+import org.apache.commons.cli.*;
 
 import java.net.InetAddress;
 import java.net.NetworkInterface;
@@ -16,20 +17,21 @@ public class Main {
     private static int subnet = 0;
     private static int universe = 0;
     private static boolean debug = false;
+    private static String networkInterface;
 
     public static void main(String[] args) throws SocketException {
         System.out.println("Hello world!");
+        commandLineParameters(args);
+
         DMXControl dmx;
         dmx = new DMXControl(0, 511);
 
-        Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
-        while (interfaces.hasMoreElements()) {
-            NetworkInterface networkInterface = interfaces.nextElement();
-            Enumeration<InetAddress> inetAddresses = networkInterface.getInetAddresses();
-            System.out.println(networkInterface);
+        if (networkInterface == null){
+            System.out.println("No network interface detected");
+            System.exit(200);
         }
 
-        NetworkInterface ni = NetworkInterface.getByName("eth6");
+        NetworkInterface ni = NetworkInterface.getByName(networkInterface);
         InetAddress address = ni.getInetAddresses().nextElement();
 
         ArtNetClient artnet = new ArtNetClient();
@@ -46,5 +48,65 @@ public class Main {
                 }
             }
         }, 0, 25, TimeUnit.MILLISECONDS);
+    }
+
+    private static void commandLineParameters(String[] args) {
+        var options = new Options()
+                .addOption("h", "help", false, "Help Message")
+                .addOption("d", "debug", false, "debug")
+                .addOption("l", "list-interfaces", false, "lists the network interfaces")
+                .addOption(Option.builder("s")
+                        .longOpt("subnet")
+                        .hasArg(true)
+                        .desc("ArtNet Subnet")
+                        .argName("0")
+                        .build())
+                .addOption(Option.builder("u")
+                        .longOpt("universe")
+                        .hasArg(true)
+                        .desc("ArtNet Universe")
+                        .argName("0")
+                        .build())
+                .addOption(Option.builder("i")
+                        .longOpt("interface")
+                        .hasArg(true)
+                        .desc("Network interface")
+                        .argName("ethx")
+                        .build());
+
+        CommandLineParser parser = new DefaultParser();
+        CommandLine line;
+        try {
+            line = parser.parse(options, args);
+            if (line.hasOption("h")) {
+                HelpFormatter formatter = new HelpFormatter();
+                formatter.printHelp("java -jar dmx.jar ", options);
+                System.exit(0);
+            }
+            if (line.hasOption("u")) {
+                universe = Integer.parseInt(line.getOptionValue("u"));
+            }
+            if (line.hasOption("s")) {
+                subnet = Integer.parseInt(line.getOptionValue("s"));
+            }
+            if (line.hasOption("d")) {
+                debug = true;
+            }
+            if (line.hasOption("i")) {
+                networkInterface = line.getOptionValue("i");
+            }
+            if (line.hasOption("l")) {
+                System.out.println("List Network Interfaces:");
+                Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
+                while (interfaces.hasMoreElements()) {
+                    NetworkInterface networkInterface = interfaces.nextElement();
+                    Enumeration<InetAddress> inetAddresses = networkInterface.getInetAddresses();
+                    System.out.println(networkInterface);
+                }
+                System.exit(201);
+            }
+        } catch (ParseException | SocketException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
